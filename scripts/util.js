7260e907ed687ab6a5c6bb7e5c704e0fa2dd75df
Erling Stage <erling.stage@gmail.com>
2021-12-25 12:33:54 +0200

    Remove unused function

--- scripts/util.js
+++ scripts/util.js
@@ -18,15 +18,13 @@ export function get_delta_time(start_time) {
     return performance.now() - start_time
 }
 
+/**
+ * Log elapsed time since start_time
+ * @param {integer} start_time in milliseconds
+ * @param {string} legend headline for measurement
+ * @returns integer time now
+ */
 export function log_time_spent(start_time, legend="elapsed time") {
     console.log(`Time in "${legend}": ${get_delta_time(start_time).toFixed(3)} millisec`)
     return performance.now()
-}
-
-
-export function log_time_spent_in(callback, legend=`${callback.name}`) {
-    const now = get_time_now()
-    callback()
-    console.log(`Time in "${legend}": ${get_delta_time(now).toFixed(3)} millisec`)
-    return now
 }
