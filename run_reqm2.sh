c49b85139a4810340fb12a2e9ff85471e37b525a
Erling Stage <erling.stage@gmail.com>
2022-02-17 18:18:32 +0200

    Run ReqM2 on scripts dir

--- run_reqm2.sh
+++ run_reqm2.sh
@@ -15,10 +15,10 @@ else
 fi
 
 # Transpile js to avoid missing ES6 compatibility in ReqM2
-./node_modules/.bin/babel.cmd scripts -d lib
+#./node_modules/.bin/babel.cmd scripts -d lib
 
 perl $REQM2 -q -i doc/requirements.xlsx -x ReqM_2::Importer::XLSX -p 'doctype=requirements;sheettitle=requirements' -O reqm2 -o requirements.reqm
-perl $REQM2 -q -i lib/*.js -x ReqM_2::Importer::TerseMultiLang -p 'doctype=sourcecode;language=JavaScript' -O reqm2 -o implementation.reqm
+perl $REQM2 -q -i scripts/*.js -x ReqM_2::Importer::TerseMultiLang -p 'doctype=sourcecode;language=JavaScript' -O reqm2 -o implementation.reqm
 perl $REQM2 -q -i test/*.js -x ReqM_2::Importer::TerseMultiLang -p 'doctype=testcode;language=JavaScript' -O reqm2 -o tests.reqm
 perl $REQM2 -q -t reqm2/*.reqm  -O reqm2 -o visual_reqm2.oreqm
 perl $REQM2 -q -r reqm2/visual_reqm2.oreqm -x ReqM_2::Exporter::Html5 -O reqm2 -o visual_reqm2.html
